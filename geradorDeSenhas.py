327227b665a6b3c06ba2e2052ab1b6fcacf98b4c
Andre <andredlima23@gmail.com>
2022-01-11 14:24:02 -0300

    finalizando o projeto

--- geradorDeSenhas.py
+++ geradorDeSenhas.py
@@ -2,7 +2,7 @@ import random, string
 
 tamanho = 16
 
-chars = string.ascii_letters + string.digits + '!@#$%¨&*(),.;:/?'
+chars = string.ascii_letters + string.digits + 'ç!@#$%¨&*(),.;:/?'
 
 rnd = random.SystemRandom()
 
